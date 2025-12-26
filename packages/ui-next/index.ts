84a80af36de24a71e457953e7136c2b4a7138e2a
Baoshuo <i@baoshuo.ren>
2025-12-26 23:38:48 +0800

    ui-next: drop @vitejs/plugin-react-swc

--- packages/ui-next/index.ts
+++ packages/ui-next/index.ts
@@ -1,7 +1,7 @@
 import fs from 'fs';
 import path from 'path';
 import importMetaUrlPlugin from '@chialab/esbuild-plugin-meta-url';
-import react from '@vitejs/plugin-react-swc';
+import react from '@vitejs/plugin-react';
 import c2k from 'koa2-connect/ts';
 import { createServer, type Plugin } from 'vite';
 import { } from '@hydrooj/framework';
